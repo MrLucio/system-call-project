3c4e3ed5af9741ee44d87880ce91bf7844879dfc
AndreaLiboni <liboni.andrea@gmail.com>
2022-05-04 15:30:15 +0200

    Non va nulla

--- system_call/server.c
+++ system_call/server.c
@@ -1,6 +1,12 @@
 /// @file sender_manager.c
 /// @brief Contiene l'implementazione del sender_manager.
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
 #include "err_exit.h"
 #include "defines.h"
 #include "shared_memory.h"
@@ -8,5 +14,30 @@
 #include "fifo.h"
 
 int main(int argc, char * argv[]) {
+    //Apertura strutture dati
+    int fifo = open("/tmp/myfifo", O_RDONLY);
+    key_t key = ftok("cio", 10);
+    printf("key = %d\n", key);
+    int shMemId = alloc_shared_memory(key, sizeof(int));
+    printf("shid = %d\n", shMemId);
+    //Ricezione numero file
+    int numFile;
+    char buffer_numFile[3];
+    read(fifo, buffer_numFile, 3);
+    numFile = atoi(buffer_numFile);
+    //invio conferma ricezione
+    int *shms = (int *)get_shared_memory(shMemId, 0);
+    if (shms == (void *)-1)
+        printf("first shmat failed\n");
+    shms = 13;
+    shmdt(shms);
+    if (shmctl(shMemId, IPC_RMID, NULL) == -1)
+        printf("shmctl failed");
+    else
+        printf("shared memory segment removed successfully\n");
+    //*shms = "13";
+
+
+    close(fifo);
     return 0;
 }
