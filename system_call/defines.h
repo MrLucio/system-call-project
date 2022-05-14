49a4aaad8d852898518897b237251ae81b114a9d
AndreaLiboni <liboni.andrea@gmail.com>
2022-05-14 10:48:42 +0200

    add define size path and chunk

--- system_call/defines.h
+++ system_call/defines.h
@@ -2,13 +2,15 @@
 /// @brief Contiene la definizioni di variabili
 ///         e funzioni specifiche del progetto.
 
+#define PATH_MAX_ELAB 1024
+#define CHUNK_MAX_ELAB 1024
 #pragma once
 #include <linux/limits.h>
 
 typedef struct message{
     int pid;
     char path[PATH_MAX];
-    char chunk[1024];
+    char chunk[CHUNK_MAX_ELAB];
 } t_message;
 
 typedef struct messageQue{
