4a14c51506b7453b124e2e9de70dc49cba17192e
AndreaLiboni <liboni.andrea@gmail.com>
2022-06-04 13:41:40 +0200

    Add function for open, close and read form message queue

---
+++ system_call/message_queue.c
@@ -0,0 +1,34 @@
+/// @file message_queue.c
+/// @brief Contiene l'implementazione delle funzioni
+///         specifiche per la gestione della MESSAGE QUEUE.
+
+#include "message_queue.h"
+#include "err_exit.h"
+#include "defines.h"
+#include  <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <sys/msg.h>
+
+// Get the message queue
+int get_message_queue(key_t key){
+    int msqid = msgget(key, IPC_CREAT | S_IRUSR | S_IWUSR);
+    if (msqid == -1)
+        ErrExit("msgget failed");
+    return msqid;
+}
+
+// Delete the message queue
+void remove_message_queue(int msqid){
+    if (msgctl(msqid, IPC_RMID, NULL) == -1) printf("msg queue already deleted\n");
+}
+
+// Read a message from message queue
+int read_message_queue(int msqid, message_t msg){
+    messageQueue_t msgQueue;
+    int response = msgrcv(msqid, &msgQueue, sizeof(message_t), 1, IPC_NOWAIT);
+    if (response != -1){
+        msg = msgQueue.msg;
+    }
+    return response;
+}
---
+++ system_call/message_queue.h
@@ -0,0 +1,16 @@
+/// @file message_queue.h
+/// @brief Contiene la definizioni di variabili e funzioni
+///         specifiche per la gestione della MESSAGE QUEUE.
+
+#pragma once
+#include <sys/types.h>
+#include "defines.h"
+
+typedef struct messageQueue{
+    long mtype;
+    message_t msg;
+} messageQueue_t;
+
+void remove_message_queue(int msqid);
+int get_message_queue(key_t key);
+int read_message_queue(int msqid, message_t msg);
